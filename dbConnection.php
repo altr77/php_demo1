2a8a62273d751f04db52290498dab417f61d0e34
altr77 <121636295+altr77@users.noreply.github.com>
2025-05-06 13:36:35 +0530

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -1,5 +1,3 @@
 <?php
-$con = new mysqli('ec2-13-201-188-250.ap-south-1.compute.amazonaws.com', 'admin', 'StrongPassword123', 'oesm');
-//all the variables defined here are accessible in all the files that include this one
-$con= new mysqli('ec2-13-201-188-250.ap-south-1.compute.amazonaws.com','admin','','oesm')or die("Could not connect to mysql".mysqli_error($con));
+$con = new mysqli('ec2-13-201-188-250.ap-south-1.compute.amazonaws.com', 'admin', 'StrongPassword123', 'oesm') or die("Could not connect to mysql".mysqli_error($con));
 ?>
